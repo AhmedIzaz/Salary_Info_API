27f8610adaf664a9aa94d1d0c1dda1082e95b71f
AhmedIzaz <ahmedizazasif@gmail.com>
2023-09-27 14:27:02 +0600

    changes some logics in if condition during query

--- src/controllers/authController/index.js
+++ src/controllers/authController/index.js
@@ -1,17 +1,22 @@
 const bcrypt = require("bcrypt");
 const db = require("../../../db");
+const jwt = require("jsonwebtoken");
 exports.signupController = async (req, reply) => {
   try {
     const { email, username, password, user_type, upper_level_user } = req.body;
-    const userExist = await db
+    const [userExist] = await db
       .promise()
-      .query(`SELECT * FROM user WHERE email=${email}`);
-    if (userExist) return reply.code(409).send("User already exists");
+      .query(`SELECT * FROM user WHERE email='${email}'`);
+
+    if (userExist?.length > 0)
+      return reply.code(409).send("User already exists");
     const hashedPassword = await bcrypt.hash(password, 10);
     await db
       .promise()
       .query(
-        `INSERT INTO user (email, username, password, user_type, upper_level_user) VALUES (${email}, ${username},${hashedPassword},${user_type},${upper_level_user})`
+        `INSERT INTO user (email, username, password, user_type, upper_level_user) VALUES ('${email}', '${username}','${hashedPassword}',${
+          user_type || null
+        },${upper_level_user || null})`
       );
     return reply.code(201).send("User created successfully");
   } catch (error) {
@@ -24,15 +29,15 @@ exports.loginController = async (request, reply) => {
     const { email, password } = request.body;
     const [user] = await db
       .promise()
-      .query(`SELECT * FROM user WHERE email=${email}`);
-    if (!user) return reply.code(409).send("User dont exists");
-    const passwordMatched = await bcrypt.compare(password, user.password);
+      .query(`SELECT * FROM user WHERE email='${email}' LIMIT 1`);
+    if (user?.length <= 0) return reply.code(409).send("User dont exists");
+    const passwordMatched = await bcrypt.compare(password, user?.[0]?.password);
     if (!passwordMatched) return reply.code(401).send("Unauthorized Access");
     const token = jwt.sign(
       {
-        userId: user?.id,
-        email: user?.email,
-        username: user?.username,
+        userId: user?.[0]?.id,
+        email: user?.[0]?.email,
+        username: user?.[0]?.username,
       },
       process.env.JWT_SECRET_KEY,
       {
